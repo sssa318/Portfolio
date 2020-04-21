7672eab87f3ae10da5bad2a28f262e00f02cc956
Saketh Ayinaparthi <57197910+sssa318@users.noreply.github.com>
2020-04-20 20:52:33 -0700

    All files of the repl.it portfolio

---
+++ index.php
@@ -0,0 +1,14 @@
+<html>
+  <head>
+    <title>Saketh's Online Porfolio</title>
+  </head>
+  <body>
+    <ul>
+      <li><a href="index.html">index.html</a></li></br>
+      <!-- Please make sure to change all the #'s to the proper link names -->
+      <li><a href="AboutMe.html">AboutMe.html</a></li></br>
+      <li><a href="Projects.html">Projects.html</a></li></br>
+      <li><a href="Resume.html">Resume.html</a></li></br>
+      <li><a href="ContactMe.html">ContactMe.html</a></li></br>
+  </body>
+</html>
